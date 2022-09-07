fd9f188521e3636cc43f84e99ab2c6d3e3e62729
Rinat <Rinat.k95@mail.ru>
2022-09-07 20:21:47 +0500

    .\README.md

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,5 +1,27 @@
-﻿//15545
-//sdfsd
-//dfggfh
+﻿
 
-///fghdgh
+
+string[] array1 = new string[6] {"222", "33", "lll", "Kazan", "4444", ""};
+string[] array2 = new string[array1.Length];
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
